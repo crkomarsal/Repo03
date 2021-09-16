772a7f6ecf16215691f85225033bfac5718d2f54
Muhamed Bajramović <muhamed.bajramovicc@gmail.com>
2021-09-16 13:54:24 +0200

    Fixing index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -8,6 +8,8 @@ function main(numOne, numTwo) {
   return difference;
 }
 
-console.log(main(numOne, numTwo)); // change this line
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
 
 module.exports = main;
