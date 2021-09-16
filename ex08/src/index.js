e7d3fbdc73ece5aa0efcdc936f159a4a93803624
Muhamed Bajramović <muhamed.bajramovicc@gmail.com>
2021-09-16 13:52:34 +0200

    Fixing index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -7,6 +7,8 @@ function main(numOne, numTwo) {
   return sum;
 }
 
-console.log(main(numOne, numTwo)); // change this line
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
 
 module.exports = main;
