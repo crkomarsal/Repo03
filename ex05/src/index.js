c912d43e4d6968af01251bfb8e77d4621402dd90
Muhamed Bajramović <muhamed.bajramovicc@gmail.com>
2021-09-16 08:57:31 +0200

    Adding ex05, ex06 and fixing index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    x = 10;
+    y = 10;
+    return [x,y];
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
